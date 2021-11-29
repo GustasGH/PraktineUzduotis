c7e26acdcf4ad2250970e2e44dca8a6f4f9f6d1f
GustasGH <gustas.brazenas@gmail.com>
2021-11-29 21:55:28 +0200

    Izambines ilgis

--- PraktineUzduots/Program.cs
+++ PraktineUzduots/Program.cs
@@ -7,8 +7,18 @@ namespace PraktineUzduots
         static void Main(string[] args)
         {
 
-            // s - plotas, v - turis, r - spindulys, a/b/c trikampio krastines
+            // Kintamieji: s - plotas, v - turis, r - spindulys, a/b/c trikampio krastines
 
+            double a, b, c, x;
+
+            a = 10;
+            b = 15;
+
+            // Istrizaines ilgis
+
+            x = a * a + b * b;
+            c = Math.Sqrt(x);
+            Console.WriteLine($"Staciojo trikampio izambines ilgis: {c}");
 
         }
     }
