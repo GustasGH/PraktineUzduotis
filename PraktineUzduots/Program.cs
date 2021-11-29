1c997877b6ba15b09cb6d5b41e4484b1d1ea3cd5
ArtStupenko <artstupenko@gmail.com>
2021-11-29 21:50:31 +0200

    Turis ir Plotas

--- PraktineUzduots/Program.cs
+++ PraktineUzduots/Program.cs
@@ -6,7 +6,24 @@ namespace PraktineUzduots
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            // s - plotas, v - turis, r - spindulys, a/b/c trikampio krastines
+            double pi = Math.PI;
+            double s;
+            double r = 6371;
+            double v;
+
+
+            //Plotas 
+
+            s = 4 * pi * (Math.Pow(r, 2));
+            v = (4 * pi * (Math.Pow(r, 3)) / 3);
+
+            Console.WriteLine($"Plotas :{Math.Round(s)}");
+            Console.WriteLine($"Turis :{Math.Round(v)}");
+
+
+
         }
     }
 }
